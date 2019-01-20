8f5fd5cb549e76b1732405e3510ec295d2ae8a5a
Mars Howard <marsbhoward@gmail.com>
2019-01-20 20:00:46 +0000

    Automatically backed up by Learn

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,11 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  it "has a name" do
-    expect(@category.name).to eq("My Category")
-  end
-
-  it "has some posts" do
-    expect(@category.posts).to include(@post)
-  end
+  #pending "add some examples to (or delete) #{__FILE__}"
 end
