30ec1ffc068f2a2d419ac197501775bf4063f698
Mars Howard <marsbhoward@gmail.com>
2019-01-20 20:17:07 +0000

    Automatically backed up by Learn

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,26 +1,28 @@
 class CategoriesController < ApplicationController
-
-  # def new
-  #   @category = Category.new
-  # end
-
-  # def create
-  #   @category = Category.new(params)
-  #   @category.save
-  #   redirect_to category_path(@category)
-  # end
+  def index
+    @categories = Category.all
+  end
 
   def show
     @category = Category.find(params[:id])
   end
 
-  # def update
-  #   @category = Category.find(params[:id])
-  #   @category.update(params.require(:category))
-  #   redirect_to category_path(@cateogory)
-  # end
+  def new
+    @category = Category.new
+  end
 
-  # def edit
-  #   @category = Category.find(params[:id])
-  # end
+  def create
+    category = Category.create(params[:category])
+    redirect_to category_path(category)
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    category = Category.find(params[:id])
+    category.update(params.require(:category))
+    redirect_to category_path(category)
+  end
 end
